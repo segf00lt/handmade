71f9043173126641c1a326421052607b4508b6a3
segf00lt <joaodear2007@gmail.com>
2026-01-08 18:51:30 -0300

    refactor some things around (following day 5)

--- win32_handmade.c
+++ win32_handmade.c
@@ -1,89 +1,133 @@
 #include "base.h"
 
 /*
+ * types
+ */
+
+typedef struct OS_Win32_Backbuffer OS_Win32_Backbuffer;
+typedef struct OS_Win32_Window_Dimensions OS_Win32_Window_Dimensions;
+
+/*
+ * structs
+ */
+
+struct OS_Win32_Backbuffer {
+  BITMAPINFO bitmap_info;
+  u8 *bitmap_memory;
+  s32 bitmap_width;
+  s32 bitmap_height;
+  u32 bytes_per_pixel;
+  u32 stride;
+};
+
+struct OS_Win32_Window_Dimensions {
+  s32 width;
+  s32 height;
+};
+
+/*
  * headers
  */
 
 LRESULT CALLBACK os_win32_main_window_callback(HWND window, UINT message, WPARAM w_param, LPARAM l_param);
-void os_win32_resize_dib_section(int width, int height);
-void os_win32_update_window(HDC device_context, RECT *window_rect, int x, int y, int width, int height);
+void os_win32_resize_dib_section(OS_Win32_Backbuffer *backbuffer, int window_width, int window_height);
+void os_win32_display_buffer_in_window(OS_Win32_Backbuffer *backbuffer, HDC device_context, int window_width, int window_height, int x, int y, int width, int height);
+OS_Win32_Window_Dimensions os_win32_get_window_dimensions(HWND window_handle);
 
 /*
  * globals
  */
 
 global b32 app_is_running;
-global BITMAPINFO bitmap_info;
-global void *bitmap_memory;
-global int bitmap_width;
-global int bitmap_height;
-global const u64 bytes_per_pixel = 4;
+global OS_Win32_Backbuffer global_backbuffer = {
+  .bytes_per_pixel = 4,
+};
 
 /*
  * functions
  */
 
-void render_weird_gradient(int x_offset, int y_offset) {
-  u64 stride = bitmap_width * bytes_per_pixel;
-
-  u8 *row = (u8*)bitmap_memory;
-  for(int y = 0; y < bitmap_height; y++) {
+void render_weird_gradient(OS_Win32_Backbuffer *backbuffer, int x_offset, int y_offset) {
+  u8 *row = (u8*)backbuffer->bitmap_memory;
+  for(int y = 0; y < backbuffer->bitmap_height; y++) {
     u32 *pixel = (u32*)row;
 
-    for(int x = 0; x < bitmap_width; x++) {
+    for(int x = 0; x < backbuffer->bitmap_width; x++) {
       //
       // pixel in memory
       //  0  1  2  3
       //  B  G  R  x
       // 00 00 00 00
 
-      u8 r = (x + x_offset);
+      u8 r = (u8)(x + x_offset);
       u8 g = 0;
-      u8 b = (y + y_offset);
+      u8 b = (u8)(y + y_offset);
 
       *pixel++ = (r << 16) | (g << 8) | b;
     }
 
-    row += stride;
+    row += backbuffer->stride;
   }
 
 }
 
-void os_win32_resize_dib_section(int width, int height) {
+OS_Win32_Window_Dimensions os_win32_get_window_dimensions(HWND window_handle) {
+  RECT client_rect;
+  GetClientRect(window_handle, &client_rect);
+
+  OS_Win32_Window_Dimensions window_dimensions = {
+    .width = client_rect.right - client_rect.left,
+    .height = client_rect.bottom - client_rect.top,
+  };
+
+  return window_dimensions;
+}
+
+void os_win32_resize_dib_section(OS_Win32_Backbuffer *backbuffer, int window_width, int window_height) {
+  int width = window_width;
+  int height = window_height;
   // TODO jfd: bulletproof this
   // maybe don't free first, free after, then free first if that fails
 
   // TODO jfd: free our DIBSection
 
-  if(bitmap_memory) {
-    os_free(bitmap_memory);
+  if(backbuffer->bitmap_memory) {
+    os_free(backbuffer->bitmap_memory);
   }
 
-  bitmap_width = width;
-  bitmap_height = height;
+  backbuffer->bitmap_width = width;
+  backbuffer->bitmap_height = height;
 
-  bitmap_info.bmiHeader.biSize = sizeof(bitmap_info.bmiHeader);
-  bitmap_info.bmiHeader.biWidth = bitmap_width;
-  bitmap_info.bmiHeader.biHeight = -bitmap_height;
-  bitmap_info.bmiHeader.biPlanes = 1;
-  bitmap_info.bmiHeader.biBitCount = 32;
-  bitmap_info.bmiHeader.biCompression = BI_RGB; // pixel format
+  backbuffer->bitmap_info.bmiHeader.biSize = sizeof(backbuffer->bitmap_info.bmiHeader);
+  backbuffer->bitmap_info.bmiHeader.biWidth = backbuffer->bitmap_width;
+  backbuffer->bitmap_info.bmiHeader.biHeight = -backbuffer->bitmap_height;
+  backbuffer->bitmap_info.bmiHeader.biPlanes = 1;
+  backbuffer->bitmap_info.bmiHeader.biBitCount = 32;
+  backbuffer->bitmap_info.bmiHeader.biCompression = BI_RGB; // pixel format
 
-  u64 bitmap_memory_size = bitmap_width*bitmap_height * bytes_per_pixel;
-  bitmap_memory = os_alloc(bitmap_memory_size);
+  u64 bitmap_memory_size = backbuffer->bitmap_width*backbuffer->bitmap_height * backbuffer->bytes_per_pixel;
+  backbuffer->bitmap_memory = os_alloc(bitmap_memory_size);
 
+  backbuffer->stride = backbuffer->bitmap_width * backbuffer->bytes_per_pixel;
 }
 
-void os_win32_update_window(HDC device_context, RECT *window_rect, int x, int y, int width, int height) {
+void os_win32_display_buffer_in_window(
+  OS_Win32_Backbuffer *backbuffer,
+  HDC device_context,
+  int window_width,
+  int window_height,
+  int x,
+  int y,
+  int width,
+  int height
+) {
 
-  int window_width = window_rect->right - window_rect->left;
-  int window_height = window_rect->bottom - window_rect->top;
   StretchDIBits(
     device_context,
-    0, 0, bitmap_width, bitmap_height,
+    0, 0, backbuffer->bitmap_width, backbuffer->bitmap_height,
     0, 0, window_width, window_height, // destination coordinates
-    bitmap_memory,
-    &bitmap_info,
+    backbuffer->bitmap_memory,
+    &backbuffer->bitmap_info,
     DIB_RGB_COLORS,
     SRCCOPY
   );
@@ -100,13 +144,10 @@ LRESULT CALLBACK os_win32_main_window_callback(
 
   switch(message) {
     case WM_SIZE: {
-      RECT client_rect;
 
-      GetClientRect(window, &client_rect);
-      int width = client_rect.right - client_rect.left;
-      int height = client_rect.bottom - client_rect.top;
+      OS_Win32_Window_Dimensions window_dimensions = os_win32_get_window_dimensions(window);
 
-      os_win32_resize_dib_section(width, height);
+      os_win32_resize_dib_section(&global_backbuffer, window_dimensions.width, window_dimensions.height);
 
       OutputDebugStringA("WM_SIZE\n");
     } break;
@@ -140,8 +181,8 @@ LRESULT CALLBACK os_win32_main_window_callback(
 
       RECT client_rect;
       GetClientRect(window, &client_rect);
-
-      os_win32_update_window(device_context, &client_rect, x, y, width, height);
+      OS_Win32_Window_Dimensions window_dimensions = os_win32_get_window_dimensions(window);
+      os_win32_display_buffer_in_window(&global_backbuffer, device_context, window_dimensions.width, window_dimensions.height, x, y, width, height);
       EndPaint(window, &paint);
     } break;
     default: {
@@ -200,7 +241,7 @@ int CALLBACK WinMain(
         }
 
         { // draw
-          render_weird_gradient(x_offset, y_offset);
+          render_weird_gradient(&global_backbuffer, x_offset, y_offset);
 
           ++x_offset;
         } // draw
@@ -208,12 +249,9 @@ int CALLBACK WinMain(
         // NOTE jfd: blit to screen
         HDC device_context;
         defer_loop(device_context = GetDC(window_handle), ReleaseDC(window_handle, device_context)) {
-          RECT client_rect;
-          GetClientRect(window_handle, &client_rect);
-          int window_width = client_rect.right - client_rect.left;
-          int window_height = client_rect.bottom - client_rect.top;
+          OS_Win32_Window_Dimensions window_dimensions = os_win32_get_window_dimensions(window_handle);
 
-          os_win32_update_window(device_context, &client_rect, 0, 0, window_width, window_height);
+          os_win32_display_buffer_in_window(&global_backbuffer, device_context, window_dimensions.width, window_dimensions.height, 0, 0, window_dimensions.width, window_dimensions.height);
         }
       }
 
